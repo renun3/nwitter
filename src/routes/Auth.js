e1840b14f4d2919941138f066a9f986f23ce520e
renun <renun0425@gmail.com>
2021-09-12 03:42:53 +0900

    4.0 Add handling login error

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,67 @@
-const Auth = () => <span>Auth</span>
+import { authService } from "fbase";
+import { useState } from "react";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data)
+        } catch (error) {
+            setError(error.message)
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email" 
+                    type="email" 
+                    placeholder="Email" 
+                    required 
+                    value={email} 
+                    onChange={onChange}
+                />
+                <input 
+                    name="password"
+                    type="password" 
+                    placeholder="Password" 
+                    required
+                    value={password}
+                    onChange={onChange} 
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <br/>
+                {error}
+            </form>
+            <div>
+                <button>Countinue with Google</button>
+                <button>Countinue with Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
